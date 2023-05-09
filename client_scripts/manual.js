e8c9bf40a5a41c244544c5cbcc0d79a76b4196e6
BuildTools <poreba.kris@gmail.com>
2023-05-09 20:10:20 +0200

    commit

---
+++ client_scripts/manual.js
@@ -0,0 +1,9 @@
+const PonderRegistry = Java.loadClass(
+  "com.simibubi.create.foundation.ponder.PonderRegistry"
+);
+
+ClientEvents.loggedIn((event) => {
+  JsonIO.write("kubejs/ponder.json", {
+    ponder: String(PonderRegistry.ALL.keySet()),
+  });
+});
